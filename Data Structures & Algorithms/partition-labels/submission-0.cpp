aa2f539e11308f0a6316a26edd07d70e0b553ff4
Nitesh Chakravarti <168835715+niteshd358@users.noreply.github.com>
2026-09-21 19:45:52 +0530

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n = s.length();
+        unordered_map<char,int> lastIndex;
+        for(int i=0; i<n; ++i){
+            lastIndex[s[i]] = i;
+        }
+        vector<int> ans;
+        int size = 0, end = 0;
+        for(int i=0; i<n; ++i){
+            size++;
+            end = max(end,lastIndex[s[i]]);
+
+            if(i == end){
+                ans.push_back(size);
+                size = 0;
+            }
+        }
+        return ans;
+    }
+};
