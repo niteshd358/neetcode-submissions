ed2522335d16fa3c531f4d7bab1fde33602f2b76
Nitesh Chakravarti <168835715+niteshd358@users.noreply.github.com>
2026-09-23 21:40:13 +0530

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.cpp
@@ -0,0 +1,36 @@
+class MinStack {
+    long mini;
+    stack<int> st;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty()){
+            st.push(0);
+            mini = val;
+        }else{
+            st.push(val-mini);
+            if(val < mini) mini = val;
+        }
+    }
+    
+    void pop() {
+        if(st.empty()){
+            return;
+        }
+        long top = st.top();
+        st.pop();
+        if(top < 0) mini = mini - top;
+    }
+    
+    int top() {
+        long top = st.top();
+        return (top >= 0) ? (top + mini) : (int)mini;
+    }
+    
+    int getMin(){
+        return (int)mini;
+    }
+};
